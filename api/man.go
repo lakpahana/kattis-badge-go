eadea1213ea81c73ec571336026c4e4563176824
lakpahana <agslakpahana@gmail.com>
2024-03-27 03:10:14 +0530

    basic api/db

--- api/man.go
+++ api/man.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"lakpahana.me/api/controllers"
+	"lakpahana.me/db"
 )
 
 type user struct {
@@ -85,9 +87,17 @@ func hello(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, "yesss")
 }
 
+func init() {
+	db.ConnectToDB()
+}
+
 func main() {
+
 	router := gin.Default()
 	router.GET("user/:id", userById)
 	router.GET("/", hello)
+	router.GET("/allUsers", controllers.GetAllUsers)
+	router.GET("/rank/:id", controllers.GetRankByCountry)
 	router.Run("localhost:8080")
+
 }
